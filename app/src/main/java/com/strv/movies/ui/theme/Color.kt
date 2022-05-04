c648ca0cdd2891e48f05564e53e00c5750777872
zak-hp <jiri.zak@bringauto.com>
2022-05-04 20:04:03 +0200

    done in strv

--- app/src/main/java/com/strv/movies/ui/theme/Color.kt
+++ app/src/main/java/com/strv/movies/ui/theme/Color.kt
@@ -1,8 +1,25 @@
 package com.strv.movies.ui.theme
-
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val md_theme_light_primary = Color(0xFFc00013)
+val md_theme_light_onPrimary = Color(0xFFffffff)
+val md_theme_light_secondary = Color(0xFF775653)
+val md_theme_light_onSecondary = Color(0xFFffffff)
+val md_theme_light_error = Color(0xFFba1b1b)
+val md_theme_light_onError = Color(0xFFffffff)
+val md_theme_light_background = Color(0xFFfcfcfc)
+val md_theme_light_onBackground = Color(0xFF211a19)
+val md_theme_light_surface = Color(0xFFfcfcfc)
+val md_theme_light_onSurface = Color(0xFF211a19)
+
+val md_theme_dark_primary = Color(0xFFffb3aa)
+val md_theme_dark_onPrimary = Color(0xFF680005)
+val md_theme_dark_secondary = Color(0xFFe7bdb8)
+val md_theme_dark_onSecondary = Color(0xFF442a27)
+val md_theme_dark_error = Color(0xFFffb4a9)
+val md_theme_dark_onError = Color(0xFF680003)
+val md_theme_dark_background = Color(0xFF211a19)
+val md_theme_dark_onBackground = Color(0xFFede0de)
+val md_theme_dark_surface = Color(0xFF211a19)
+val md_theme_dark_onSurface = Color(0xFFede0de)

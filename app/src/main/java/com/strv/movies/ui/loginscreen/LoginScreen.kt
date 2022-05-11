ac035cec2e3a882b6ca00cacc6a92669c8b45f6c
zak-hp <jiri.zak@bringauto.com>
2022-05-11 10:38:55 +0200

    update login page

--- app/src/main/java/com/strv/movies/ui/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/strv/movies/ui/loginscreen/LoginScreen.kt
@@ -7,60 +7,88 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.strv.movies.ui.theme.md_theme_light_primary
 
 @Composable
 fun LoginScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = "Movies app Login")
-        var loginEmail by remember { mutableStateOf(TextFieldValue("")) }
-        OutlinedTextField(
-            value = loginEmail,
-            label = { Text(text = "Email") },
-            onValueChange = { newText ->
-                loginEmail = newText
-            })
-        var loginPassword by remember { mutableStateOf(TextFieldValue("")) }
-        var passwordVisible by rememberSaveable { mutableStateOf(false) }
-        OutlinedTextField(
-            value = loginPassword,
-            label = { Text(text = "Password") },
-            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-            onValueChange = { newText ->
-                loginPassword = newText
-            })
-        Button(
-            onClick = { }, modifier = Modifier
-                .padding(8.dp)
-                .size(width = 150.dp, height = 35.dp)
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(top = 100.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Login")
+            LoginText()
+            LoginEmail()
+            LoginPassword()
+            LoginButton(text = "Sign in", color = md_theme_light_primary)
+
         }
-        Button(
-            onClick = { },
+
+        Row(
             modifier = Modifier
-                .padding(8.dp)
-                .size(width = 150.dp, height = 35.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+                .weight(1f, false)
         ) {
-            Text(text = "Google")
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "or sign in with")
+                LoginButton("Google", Color.White)
+                LoginButton("Facebook", Color(0xFF009CFF))
+            }
         }
-        Button(
-            onClick = { }, modifier = Modifier
-                .padding(8.dp)
-                .size(width = 150.dp, height = 35.dp),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color(0xFFFF2400)
-            )
-        ) {
-            Text(text = "Facebook")
-        }
+    }
+}
+
+@Composable
+fun LoginText() {
+    Text(text = "Movies app Login", fontSize = 30.sp, fontStyle = FontStyle.Italic)
+}
+
+@Composable
+fun LoginEmail() {
+    var loginEmail by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        value = loginEmail,
+        label = { Text(text = "Email") },
+        onValueChange = { newText ->
+            loginEmail = newText
+        })
+}
+
+@Composable
+fun LoginPassword() {
+    var loginPassword by remember { mutableStateOf(TextFieldValue("")) }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+    OutlinedTextField(
+        value = loginPassword,
+        label = { Text(text = "Password") },
+        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        onValueChange = { newText ->
+            loginPassword = newText
+        })
+}
+
+@Composable
+fun LoginButton(text: String, color: Color) {
+    Button(
+        onClick = { }, modifier = Modifier
+            .padding(8.dp)
+            .size(width = 150.dp, height = 35.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = color
+        )
+    ) {
+        Text(text = text)
     }
 }
